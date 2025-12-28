9776a7a350e037e5d9ce1fb513fabc7cb5d28757
HardikKaran <hardik.karan24@imperial.ac.uk>
2025-12-28 22:24:25 +0530

    declaration of DataHandler and what a bar will look like and functions related to it

--- backtester/data_handler.h
+++ backtester/data_handler.h
@@ -11,3 +11,21 @@
 
 #include <event.h>
 
+struct Bar {
+  std::string symbol;
+  std::chrono::year_month_day date;
+  double open;
+  double high;
+  double low;
+  double close;
+  double vol;
+};
+
+class DataHandler {
+  public:
+    virtual ~DataHandler() = default;
+
+    virtual std::vector<Bar> getLatestBars(std::string symbol, int N = 1) = 0;
+    
+    virtual void updateBars() = 0;
+};

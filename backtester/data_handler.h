8f003ff849c37e6149a013fc7efe505eb8d6c161
HardikKaran <hardik.karan24@imperial.ac.uk>
2025-12-28 22:10:37 +0530

    initialised data_handler class

---
+++ backtester/data_handler.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+#include <map>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <algorithm>
+#include <utility>
+
+#include <event.h>
+

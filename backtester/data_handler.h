cb4e73fd3ff449ff6c304e1906e32a76b0aa9b67
HardikKaran <hardik.karan24@imperial.ac.uk>
2025-12-29 19:06:11 +0530

    added framework for collecting CSV info of company

--- backtester/data_handler.h
+++ backtester/data_handler.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <queue>
+#include <memory>
 #include <fstream>
 #include <sstream>
 #include <chrono>
@@ -22,10 +24,50 @@ struct Bar {
 };
 
 class DataHandler {
+  /*
+  The goal of a (derived) DataHandler object is to output a generated
+  set of bars (OLHCVI) for each symbol requested. 
+
+  This will replicate how a live strategy would function as current
+  market data would be sent "down the pipe". Thus a historic and live
+  system will be treated identically by the rest of the backtesting suite.
+  */
+
   public:
     virtual ~DataHandler() = default;
 
+    /*
+    Returns the last N bars from the latest_symbol list,
+    or fewer if less bars are available.
+    */
     virtual std::vector<Bar> getLatestBars(std::string symbol, int N = 1) = 0;
     
+    /*
+    Pushes the latest bar to the latest symbol structure
+    for all symbols in the symbol list.
+    */
     virtual void updateBars() = 0;
+};
+
+class HistoricCSVDataHandler : public DataHandler {
+  public:
+    HistoricCSVDataHandler(std::queue<std::shared_ptr<Event>>& events, 
+      std::string csvDir, std::vector<std::string> symbolList);
+
+    std::vector<Bar> getLatestBars(std::string symbol, int N = 1) override;
+
+    void updateBars() override;
+
+  private: 
+    std::queue<std::shared_ptr<Event>>& events;
+    std::string csvDir;
+    std::vector<std::string> symbolList;
+    std::map<std::string, std::vector<Bar>> symbolData;
+    std::map<std::string, std::vector<Bar>> latestSymbolData;
+    bool contBacktest = true;
+
+    void openConvertCSVFiles();
+
+    std::map<std::string, int> barIndex; // Tracks where in list
+    bool getNextBar(const std::string& symbol, Bar& outBar); // Get next bar for specific symbol
 };

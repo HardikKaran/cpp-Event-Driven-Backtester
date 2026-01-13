20ae465b2358d8dd558eeb795fb0d6449f13362c
HardikKaran <hardik.karan24@imperial.ac.uk>
2026-01-13 21:23:51 +0000

    generated calculateSignals helper function that buys any stock at the latest price when called in the main.cpp file

--- backtester/strategy.cpp
+++ backtester/strategy.cpp
@@ -11,3 +11,34 @@ BuyAndHoldStrategy::BuyAndHoldStrategy(DataHandler* data,
             boughtStatus[s] = false;
         }
     }
+
+void BuyAndHoldStrategy::calculateSignals() {
+    /*
+    For "Buy and Hold" we generate a single signal per symbol
+    and then no additional signals. This means we are 
+    constantly long the market from the date of strategy
+    initialisation.
+
+    Called in main.cpp when MarketEvent occurred
+    */
+    for (const auto& s : symbolList) {
+        std::vector<Bar> bars = data->getLatestBars(s, 1);
+
+        if (!bars.empty()) {
+            if (!boughtStatus[s]) {
+                Bar latestBar = bars[0];
+
+                auto signal = std::make_shared<SignalEvent>(
+                    latestBar.symbol, 
+                    latestBar.date, 
+                    SignalType::LONG
+                );
+
+                events.push(signal);
+                boughtStatus[s] = true;
+                
+                std::cout << "LONG " << s << " at " << latestBar.close << std::endl;
+            }
+        }
+    }
+}

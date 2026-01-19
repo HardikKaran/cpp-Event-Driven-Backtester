5716d163b84f00edc964fc466b82d3f2c875b9d8
HardikKaran <hardik.karan24@imperial.ac.uk>
2026-01-19 20:44:06 +0000

    implemented portfolio classes

--- backtester/portfolio.cpp
+++ backtester/portfolio.cpp
@@ -8,10 +8,46 @@ NaivePortfolio::NaivePortfolio(DataHandler* bars,
                                double initialCapital)
     : bars(bars), events(events), startDate(startDate), initialCapital(initialCapital) {
 
-    this->symbolList = bars->getSymbols(); 
+    this->symbolList = bars->getSymbolList(); 
 
     // Initialize the tracking containers
     constructAllPositions();
     constructCurrentHoldings();
     constructAllHoldings();
+}
+
+void NaivePortfolio::constructAllPositions() {
+    std::map<std::string, long> initialPos;
+
+    for (const auto& sL : symbolList) {
+        initialPos[sL] = 0;
+    }
+
+    currentPositions = initialPos;
+
+    allPositions.push_back(initialPos);
+}
+
+void NaivePortfolio::constructCurrentHoldings() {
+    for (const auto& sL : symbolList) {
+        currentHoldings[sL] = 0.0;
+    }
+
+    currentHoldings["cash"] = initialCapital;
+    currentHoldings["commission"] = 0.0;
+    currentHoldings["total"] = initialCapital;
+}
+
+void NaivePortfolio::constructAllHoldings() {
+    std::map<std::string, double> initialHoldings;
+
+    for (const auto& symbol : symbolList) {
+        initialHoldings[symbol] = 0.0;
+    }
+
+    initialHoldings["cash"] = initialCapital;
+    initialHoldings["commission"] = 0.0;
+    initialHoldings["total"] = initialCapital;
+
+    allHoldings.push_back(initialHoldings);
 }
